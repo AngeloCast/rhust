00528fc95e0320a6092b0929128245afee0070d9
Castillo Angelo <82747051+AngeloCast@users.noreply.github.com>
2023-01-06 10:10:14 +0800

    event
    event

--- app/models/Event_model.php
+++ app/models/Event_model.php
@@ -66,13 +66,12 @@ class Event_model extends Model{
     }
 
     public function upload(){
-        $random = 'File' . rand(0, 100000);
         $target_dir = "public/images/";
-        $target_file = $random . basename($_FILES["fileToUpload"]["name"]);
+        $target_file = basename($_FILES["fileToUpload"]["name"]);
         $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-                            // Check if image file is a actual image or fake image
+        // Check if image file is a actual image or fake image
         if(isset($_POST["submit"])) {
             $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
             if($check !== false) {
@@ -101,8 +100,9 @@ class Event_model extends Model{
         } 
         else 
         {
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir . $target_file)) {
-                return $target_file;
+            $new_img_name = uniqid('IMG-', true).'.'.$imageFileType;
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir . $new_img_name)) {
+                return $new_img_name;
             }
         }
     }
